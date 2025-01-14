1ba1c9242ed5eef93e8977e7704babb23b1259ad
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-01-14 20:25:34 +0530

    Fixed Login router

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,8 @@
 "use client"
-import { useRouter } from "next/navigation";
 import LogIn from "./LogIn/page";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [visibleContent, setVisibleContent] = useState<boolean>(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const authorized = async () => {
-      const cookies = document.cookie.split(";");
-      const cookie = cookies.find((cookie) => cookie.startsWith("authtoken="));
-      const mainCookie = cookie ? cookie.split("=")[1] : null;
-
-      if (!mainCookie) {
-        setVisibleContent(true);
-        return;
-      }
-
-      try {
-        const response = await fetch("https://nexdesk-backend.onrender.com/userAuthenticated", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${mainCookie}`
-          },
-          credentials: "include"
-        })
-
-        if (!response.ok) {
-          router.push("/");
-          setVisibleContent(true);
-        }
-        else {
-          setVisibleContent(false);
-          router.push("/Home");
-        }
-      } catch (error) {
-        console.log("error: ", error);
-        setVisibleContent(true)
-      }
-    };
-
-    authorized();
-  }, [router]);
-
   return (
-    visibleContent &&
     <>
       <LogIn />
     </>
